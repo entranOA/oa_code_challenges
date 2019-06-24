bbe4be202495c4220a4bede3eb649c160622226b
Arctor24 <alex@outsideanalytics.com>
2019-06-24 17:11:33 -0600

    Simple solution

---
+++ 6-24-19/ajr_solution.py
@@ -0,0 +1,9 @@
+test1 = [10, 15, 3, 7]
+goal_sum = 17
+
+def sum_in_list(in_list: list, goal_sum: int) -> bool:
+    for i in in_list:
+        for j in in_list:
+            if i + j == goal_sum:
+                return True
+return False
